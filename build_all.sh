074578b88ce3208432ef8492496e2d7dc7c81060
zigen <hrlclb@gmail.com>
2021-07-25 17:40:30 +0900

    fix build script

--- build_all.sh
+++ build_all.sh
@@ -3,11 +3,12 @@ set -eux
 
 if [ -f "/.dockerenv" ]; then
 bash build_qt.sh
-cd omnetpp
-source setenv
+cd /root/omnetpp
+source ./setenv
 cp configure.user.dist.wasm configure.user
 emconfigure ./configure
-emmake make -j4
+emmake make -j4 common layout eventlog scave nedxml sim envir utils qtenv
+emmake make -j4 aloha canvas cqn dyna fifo hypercube histograms neddemo queueinglib queueinglibext routing tictoc
 else
 docker build -t quisp-on-wasm2 .
 docker run --rm -it --workdir=/root/ -v `pwd`:/root/ quisp-on-wasm2 bash -c "cd /root/ && ./build_all.sh"
