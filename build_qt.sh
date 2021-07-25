ea5022a4f36b63ed44938518a69020d8ef64ed0d
zigen <hrlclb@gmail.com>
2021-07-25 17:39:50 +0900

    disable qtbase example compile

--- build_qt.sh
+++ build_qt.sh
@@ -3,6 +3,6 @@
 set -eux 
 
 cd qtbase
-./configure -xplatform wasm-emscripten -prefix $PWD/qtbase -opensource -confirm-license
+./configure -xplatform wasm-emscripten -prefix $PWD/qtbase -opensource -no-compile-examples -confirm-license
 make -j4
 make install
