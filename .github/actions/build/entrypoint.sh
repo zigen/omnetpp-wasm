18c3429fb76b89b3ad083a181789de10834e5f90
zigen <hrlclb@gmail.com>
2021-08-30 01:04:49 +0900

    fix path

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -5,4 +5,6 @@ echo $HOME
 cp /github/workspace/.bashrc $HOME/
 ln -s /github/workspace/omnetpp $HOME/omnetpp
 ln -s /github/workspace/qtbase $HOME/qtbase
+ln -s /github/workspace/qtbase /root/qtbase
+export PATH=$PATH:$HOME/qtbase/qtbase/bin
 bash build_all.sh
