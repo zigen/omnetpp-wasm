10257685b16b62eadc5a123cd0f91e2ccbf57d18
zigen <hrlclb@gmail.com>
2021-08-30 11:22:15 +0900

    fix link

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -2,9 +2,7 @@
 set -eux
 echo "hello world"
 echo $HOME
-cp /github/workspace/.bashrc $HOME/
-ln -s /github/workspace/omnetpp $HOME/omnetpp
-ln -s /github/workspace/qtbase $HOME/qtbase
+ln -s /github/workspace/ $HOME/
 ln -s /github/workspace/qtbase /root/qtbase
 export PATH=$PATH:$HOME/qtbase/qtbase/bin
 bash build_all.sh
