a20a705561e62565b6f3b923eb6eacb0023e22ac
zigen <hrlclb@gmail.com>
2021-08-30 11:45:17 +0900

    fix home

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -eux
 echo "hello world"
-echo $HOME
-ln -s /github/workspace $HOME
-ls -la $HOME
 ln -s /github/workspace/qtbase /root/qtbase
+export PATH=/github/workspace/
 export PATH=$PATH:$HOME/qtbase/qtbase/bin
 bash build_all.sh
