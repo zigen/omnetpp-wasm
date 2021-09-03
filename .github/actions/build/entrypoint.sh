a27752417862e23a29cae89a4f0a644269c7108a
zigen <hrlclb@gmail.com>
2021-09-04 01:22:06 +0900

    cleanup build script

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -eux
-echo "hello world"
 ln -s /github/workspace/qtbase /root/qtbase
 export HOME=/github/workspace/
 export PATH=$PATH:$HOME/qtbase/qtbase/bin
--- build_all.sh
+++ build_all.sh
@@ -3,15 +3,20 @@ set -ex
 
 if [ -f "/.dockerenv" ]; then
 source ~/.bashrc
+echo "::group::Building Qt"
 bash build_qt.sh
+echo "::endgroup::"
+echo "::group::Building OMNeT++"
 cd ~/omnetpp
 cp configure.user.dist.wasm configure.user
 export PATH=$HOME/omnetpp/bin:$PATH
 export QT_SELECT=5
-qmake -query
 emconfigure ./configure
 emmake make -j4 common layout eventlog scave nedxml sim envir utils qtenv
+echo "::endgroup::"
+echo "::group::Building OMNeT++ Samples"
 emmake make aloha canvas cqn dyna fifo hypercube histograms neddemo queueinglib queueinglibext routing tictoc
+echo "::endgroup::"
 else
 docker build -t quisp-on-wasm2 .
 docker run --rm -it --workdir=/root/ -v `pwd`:/root/ quisp-on-wasm2 bash -c "cd /root/ && ./build_all.sh"
