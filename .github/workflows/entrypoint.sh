5ca96023c1a25fd3aac2ff4a90dfb08764ed8670
zigen <hrlclb@gmail.com>
2021-09-05 01:43:31 +0900

    remove entrypoint

--- .github/workflows/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eux
-ln -s /github/workspace/qtbase /root/qtbase
-mkdir /github/workspace/omnetpp/bin
-mv /usr/local/bin/opp_* /github/workspace/omnetpp/bin/
-export HOME=/github/workspace/
-export PATH=$PATH:$HOME/qtbase/qtbase/bin
-bash build_all.sh
