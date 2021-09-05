04365dd564b21ca1ab7fa3646b839f3ba3142a91
zigen <hrlclb@gmail.com>
2021-09-05 13:34:55 +0900

    disable example build

--- build_all.sh
+++ build_all.sh
@@ -30,18 +30,18 @@ if [ ! -d ~/omnetpp/lib ]; then
 	echo "::endgroup::"
 fi
 
-echo "::group::Building OMNeT Samples"
-cd ~/omnetpp
-# TARGETS="aloha canvas cqn dyna fifo hypercube histograms neddemo queueinglib queueinglibext routing tictoc"
-TARGETS="aloha canvas cqn dyna fifo hypercube histograms neddemo routing tictoc"
-emmake make $TARGETS
-SAMPLE_OUT=~/sample_outputs
-for target in $TARGETS; do
-	cd ~/omnetpp/samples/$target
-	emmake make index.html
-	mkdir -p $SAMPLE_OUT/$target
-	cd out/emcc-release/
-	cp *.js *.wasm *.data qtlogo.svg index.html $SAMPLE_OUT/$target/
-done
-echo "::endgroup::"
+# echo "::group::Building OMNeT Samples"
+# cd ~/omnetpp
+# # TARGETS="aloha canvas cqn dyna fifo hypercube histograms neddemo queueinglib queueinglibext routing tictoc"
+# TARGETS="aloha canvas cqn dyna fifo hypercube histograms neddemo routing tictoc"
+# emmake make $TARGETS
+# SAMPLE_OUT=~/sample_outputs
+# for target in $TARGETS; do
+# 	cd ~/omnetpp/samples/$target
+# 	emmake make index.html
+# 	mkdir -p $SAMPLE_OUT/$target
+# 	cd out/emcc-release/
+# 	cp *.js *.wasm *.data qtlogo.svg index.html $SAMPLE_OUT/$target/
+# done
+# echo "::endgroup::"
 
