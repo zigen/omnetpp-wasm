edcd8859cb5b1a857e2cda51786829f74472662e
zigen <hrlclb@gmail.com>
2021-09-28 10:44:53 +0900

    update omnetpp and build script

--- build.py
+++ build.py
@@ -64,37 +64,10 @@ def mkdir(dir):
         os.makedirs(dir)
 
 
-def copy_artifacts(files, dest_dir):
-    for file in files:
-        shutil.copyfile(
-            os.path.join("out/emcc-release/", file), os.path.join(dest_dir, file)
-        )
-
-def copy_qt_res(source, target, output_folder):
-    global QT_RESOURCE_DIR
-    dest_dir = os.path.join(output_folder, target)
-    shutil.copyfile(os.path.join(QT_RESOURCE_DIR, source), dest_dir)
-
-def copy_and_replace_qt_res(source, target, output_folder):
-    dest_dir = os.path.join(output_folder, target)
-    copy_qt_res(source, target, output_folder)
-    with open(dest_dir, "r") as f:
-        body = f.read()
-
-    body = re.sub("@APPNAME@", "target", body)
-    with open(dest_dir, "w") as f:
-        f.write(body)
-
-
 config = parse_config()
 
-QT_RESOURCE_DIR = "/root/qtbase/qtbase/plugins/platforms/"
-LIBS = "--post-js post.js -L/root/omnetpp/lib -Wl,--whole-archive -loppmain -L/root/qtbase/qtbase/lib -L/root/qtbase/qtbase/plugins/platforms -L/root/qtbase/qtbase/plugins/imageformats/ -lqwasm -lQt5Gui -lQt5Core -lQt5Widgets -lQt5PrintSupport -lQt5OpenGL -lQt5EventDispatcherSupport -lQt5FontDatabaseSupport -lqtfreetype -lqgif  -lqtpcre2 -lqtlibpng -lqtharfbuzz -lqjpeg -lqico -loppqtenv -loppenvir -lopplayout  -loppsim -loppnedxml -loppcommon  -lstdc++".split()
-COPTS = "-O3 -Os -g0".split()
-WASM_OPTS = '--bind -s ASYNCIFY=1 -s USE_PTHREADS=0 -s PTHREAD_POOL_SIZE=1 -s TOTAL_MEMORY=1GB -s WASM=1 -s FULL_ES2=1 -s USE_WEBGL2=1 -s NO_EXIT_RUNTIME=0  -s EXPORTED_RUNTIME_METHODS=["UTF16ToString","stringToUTF16","ccall","cwrap"] -s DISABLE_EXCEPTION_CATCHING=0'.split()
-EMCC = "/emsdk/upstream/emscripten/em++"
-OUT = ["-o", "out/emcc-release/target.html"]
-
+COPTS = "-O3 -Os -s ASSERTIONS=2 -s SAFE_HEAP=1".split()
+WASM_OPTS = '--bind -s ASYNCIFY=1 -s USE_PTHREADS=0 -s PTHREAD_POOL_SIZE=1 -s TOTAL_MEMORY=1GB -s WASM=1 -s FULL_ES2=1 -s USE_WEBGL2=1 -s NO_EXIT_RUNTIME=0 -s EXPORTED_RUNTIME_METHODS=["UTF16ToString","stringToUTF16","ccall","cwrap"] -s DISABLE_EXCEPTION_CATCHING=0 --post-js post.js'.split()
 
 files = []
 
@@ -105,18 +78,7 @@ for file in config["ned-files"] + [
 ]:
     files.append("--preload-file")
     files.append(file)
-
+files_str = " ".join(files)
 gen_post_js(config)
 subprocess.run(["opp_makemake", "-f", "-M", "release", "--deep", "-i", "./makefrag"])
-subprocess.run(["emmake", "make"])
-OBJS = ["-Wl,--whole-archive"] + glob("out/emcc-release/**/*.o", recursive=True)
-print("OBJS: ", OBJS)
-print([EMCC] + OUT + OBJS + LIBS + WASM_OPTS + files)
-subprocess.run([EMCC] + OUT + OBJS + LIBS + WASM_OPTS + files)
-
-output_folder = config["output-folder"]
-mkdir(output_folder)
-copy_artifacts(["target.wasm", "target.js", "target.data"], output_folder)
-copy_and_replace_qt_res("wasm_shell.html", "index.html", output_folder)
-copy_and_replace_qt_res("qtloader.js", "qtloader.js", output_folder)
-copy_qt_res("qtlogo.svg", "qtlogo.svg", output_folder)
+subprocess.run(["emmake", "make", "index.html", f'PRELOAD_FILES={files_str}'])
--- build_qtenv.sh
+++ build_qtenv.sh
@@ -4,4 +4,5 @@ cd omnetpp
 rm -f out/emcc-debug/src/qtenv/liboppqtenv*
 rm -f lib/liboppqtenv*
 cd src/qtenv
-emmake make MODE=debug -j8
+#emmake make MODE=debug -j8
+emmake make MODE=release -j8
