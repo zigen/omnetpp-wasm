4262d10e01ae2464684d13f72cb7dde9f294aa72
zigen <hrlclb@gmail.com>
2022-02-15 12:51:39 +0900

    fix node template

--- node_exec_template.js
+++ node_exec_template.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 const fs = require("fs");
+process.chdir(__dirname);
 const header = `
 Module = {
     arguments: ${JSON.stringify(process.argv.splice(2))},
 };
 `;
 const src = fs.readFileSync("./@APPNAME@.js", { encoding: "utf8" });
-eval(header + src)
+eval(header + src.substr(0,src.indexOf(";arguments_")))
