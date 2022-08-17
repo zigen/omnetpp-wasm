7ad77872635309f5c24c681c0d91e6f41e1b3422
zigen <hrlclb@gmail.com>
2022-08-18 06:38:48 +0900

    fix vite config

--- opp-wasm-app/vite.config.js
+++ opp-wasm-app/vite.config.js
@@ -1,25 +1,29 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import {resolve} from "path"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte({ hot: false })],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/main.js"),
-      name: "opp-wasm-client",
-      format: "esm",
-    },
-    emptyOutDir: false,
-    outDir: "../omnetpp/samples/tictoc/out/emcc-release",
-    watch: {},
-    minify: false,
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`
+export default defineConfig(({ command, mode }) => {
+
+  const isProduction = mode === "production"
+  return {
+    plugins: [svelte({ hot: false })],
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/main.js"),
+        name: "opp-wasm-client",
+        format: "esm",
+      },
+      emptyOutDir: false,
+      outDir: "../dist",
+      watch: isProduction ? null : {},
+      minify: isProduction,
+      rollupOptions: {
+        output: {
+          entryFileNames: `[name].js`,
+          chunkFileNames: `[name].js`,
+          assetFileNames: `[name].[ext]`
+        }
       }
     }
   }
